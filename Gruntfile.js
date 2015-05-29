572993e87f8cdf15ba58d7dbb43afde8fdf892e1
nobody <411298027@qq.com>
2015-05-29 14:16:06 +0800

    add min.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,19 @@ module.exports=function(grunt){
                 src: 'less/nobody.less',
                 dest: 'dist/nobody.css'
             }
+        },
+        cssmin:{
+            options:{
+                keepSpecialComments: false
+            },
+            build:{
+                src: 'dist/nobody.css',
+                dest: 'dist/nobody.min.css'
+            }
         }
     });
     require('load-grunt-tasks')(grunt);
     require('time-grunt')(grunt);
 
-    grunt.registerTask('default',['less']);
+    grunt.registerTask('default',['less','cssmin']);
 }
